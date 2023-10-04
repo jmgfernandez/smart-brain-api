49ebe3fc1cc9010f2c8477a0587eee2686bc8197
Migz <juanmiguelganfernandez@gmail.com>
2023-10-04 12:06:17 +0800

    Connect DB to back-end using KNEX
    Use bcrypt to hash passwords and compare plaintext passwords to them
    
    Create controllers for logic contained in routes/endpoints
    
    Move Clarifai API request to back-end so PAT isn't visible in request header

---
+++ controllers/image.js
@@ -0,0 +1,58 @@
+const handleImage = (req, res, db) => {
+    const { id } = req.body;
+    db('users').where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => {
+        res.json(entries[0].entries);
+    })
+    .catch(err => res.status(400).json('unable to get entries'));
+}
+
+const returnClarifaiRequestOptions = (imageUrl) => {
+    const PAT = 'bfce70bf06564e5aa765bf56460e8ebb';
+    const USER_ID = 'jmgfernandez';       
+    const APP_ID = 'test';
+    const IMAGE_URL = imageUrl;
+    
+    const raw = JSON.stringify({
+        "user_app_id": {
+            "user_id": USER_ID,
+            "app_id": APP_ID
+        },
+        "inputs": [
+            {
+                "data": {
+                    "image": {
+                        "url": IMAGE_URL
+                    }
+                }
+            }
+        ]
+    });
+
+    const requestOptions = {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Authorization': 'Key ' + PAT
+        },
+        body: raw
+    };
+
+    return requestOptions;
+};
+
+const handleApiCall = (req, res) => {
+    
+    const MODEL_ID = 'face-detection';
+    fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/outputs", returnClarifaiRequestOptions(req.body.input))
+    .then(response => response.json())
+    .then(data => res.json(data))
+    .catch(err => res.status(400).json('unable to work with API'))
+}
+
+module.exports = {
+    handleImage,
+    handleApiCall
+}
